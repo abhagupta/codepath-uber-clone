030d1d0f8cd96edd92c27e2c3986c4db82313c24
Abha Gupta <agupta3@walmart.com>
2015-05-28 15:18:55 -0700

    adding user stories and specs

--- routes.js
+++ routes.js
@@ -1,3 +1,28 @@
 module.exports = (app) => {
 	app.get('/', (req, res) => res.render('index.ejs'))
+
+	app.post('/' , (req, res) => {
+		let address = req.body.address
+		console.log("address: " + address);
+		//to do add the logic to find the nearest cabs 
+
+		res.redirect('/nearestCabs/'+address)
+
+	})
+
+	app.get('/nearestCabs/:address' , (req, res) => {
+		
+		// code to fetch from google maps 
+		// parse the response
+		// do the calculation
+		// create the objects of cabs
+		// render them on the google maps
+		console.log("Paramter address :" + req.params.address);
+
+		res.render('nearestCabs.ejs', {
+			address: address
+		})
+	})
+
+
 }
