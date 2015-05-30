46bb3018bae82df9c976f1b70fa6885788f66fe7
Abha Gupta <agupta3@walmart.com>
2015-05-29 21:41:28 -0700

    session added with rider information

--- README.md
+++ README.md
@@ -8,3 +8,23 @@ The application does following :
 - Find nearest cabs/drivers who have registered themselves in the database
 - Lets riders select the most nearest cab
 - Lets drivers accept or reject the request from the rider
+
+##Rider Registration
+Rider registers himself with three values :
+	- rider name
+	- password
+
+For simplicity, any rider is registered as rider, with duplicate rider name allowed.
+
+The authentication is achieved to maintain the rider information in session.
+
+##Driver Registration
+
+Driver are registered with three values :
+
+- cabId
+- latitude
+- longitide
+
+Driver are not maintained in session
+
--- routes.js
+++ routes.js
@@ -10,14 +10,16 @@ let Driver = require('./model/driver')
 let googleKey = 'AIzaSyBoQL7KUGLHgnh_Ws9ye-HrfoZYPUuJpFM'
 
 function _2dArraySort(a, b) {
-	if (a[0] === b[0]) {
-		return 0;
-	} else {
-		return (a[0] < b[0]) ? -1 : 1;
-	}
+    if (a[0] === b[0]) {
+        return 0;
+    } else {
+        return (a[0] < b[0]) ? -1 : 1;
+    }
 }
 
 module.exports = (app) => {
+    let passport = app.passport
+
     app.get('/', (req, res) => res.render('index.ejs'))
 
     app.get('/findCabs', (req, res) => res.render('findCabs.ejs'))
@@ -26,78 +28,89 @@ module.exports = (app) => {
         // address should be formatted like:
         // 850+Cherry+Ave.+San+Bruno,+CA+94066
         let address = parseAddressForGoogle(req.body.address)
-        //to do add the logic to find the nearest cabs 
+            //to do add the logic to find the nearest cabs 
         res.redirect('/nearestCabs/' + address)
     })
 
-	// TODO: pin the RIDER on the map as well!!
-	// Michael + view (nearestCabs)
-	app.get('/nearestCabs/:address', then(async(req, res) => {
-		// code to fetch from google maps 
-		// parse the response
-		// do the calculation
-		// create the objects of cabs
-		// render them on the google maps
-		let userAddress = req.params.address
-		console.log("User address :" + userAddress);
-		try {
-			let resp = await rp({
-				uri: `https://maps.googleapis.com/maps/api/geocode/json?address=${userAddress}&key=${googleKey}`,
-				resolveWithFullResponse: true
-			})
-			let results = JSON.parse(resp.body)
-			console.log('lat = ', results.results[0].geometry.location.lat)
-			console.log('lng = ', results.results[0].geometry.location.lng)
-			let userLat = results.results[0].geometry.location.lat
-			let userLng = results.results[0].geometry.location.lng
-
-			let drivers = await Driver.promise.find({})
-
-			let start = {
-				latitude: userLat,
-				longitude: userLng
-			}
-
-			var availDrivers = []
-
-			_.forEach(drivers, function(record, key) {
-				let end = {
-					latitude: record.latitude,
-					longitude: record.longitude
-				}
-				let distance = haversine(start, end)
-
-				console.log('Driver :: ', record.cabid + ', Distance :: ' + distance)
-				availDrivers.push([distance.toFixed(2), record.cabid, record.latitude, record.longitude])
-			});
-			availDrivers = availDrivers.sort(_2dArraySort)
-			// console.log(availDrivers)
-			let sortedDrivers = []
-			for (let i=0; i<availDrivers.length; i++) {
-				sortedDrivers.push({"cabid":availDrivers[i][1], "distance":availDrivers[i][0], "lat":availDrivers[i][2], "lng":availDrivers[i][3]})
-			}
 
 
-			// var sortedDriversJson = multiDimensionArray2JSON(availDrivers)
-			console.log(sortedDrivers)
-			res.render('nearestCabs.ejs', {
-				drivers: sortedDrivers,
-				rider: {"lat": userLat, "lng": userLng}
-			})
+    // TODO: pin the RIDER on the map as well!!
+    // Michael + view (nearestCabs)
+    app.get('/nearestCabs/:address', then(async(req, res) => {
+            // code to fetch from google maps 
+            // parse the response
+            // do the calculation
+            // create the objects of cabs
+            // render them on the google maps
+            console.log("********************** Session User Id  ******************************: " +  req.user._id)
+            let userAddress = req.params.address
+            console.log("User address :" + userAddress);
+            try {
+                let resp = await rp({
+                    uri: `https://maps.googleapis.com/maps/api/geocode/json?address=${userAddress}&key=${googleKey}`,
+                    resolveWithFullResponse: true
+                })
+                let results = JSON.parse(resp.body)
+                console.log('lat = ', results.results[0].geometry.location.lat)
+                console.log('lng = ', results.results[0].geometry.location.lng)
+                let userLat = results.results[0].geometry.location.lat
+                let userLng = results.results[0].geometry.location.lng
 
-		} catch (e) {
-			console.log(e)
-		}
+                let drivers = await Driver.promise.find({})
+
+                let start = {
+                    latitude: userLat,
+                    longitude: userLng
+                }
+
+                var availDrivers = []
+
+                _.forEach(drivers, function(record, key) {
+                    let end = {
+                        latitude: record.latitude,
+                        longitude: record.longitude
+                    }
+                    let distance = haversine(start, end)
+
+                    console.log('Driver :: ', record.cabid + ', Distance :: ' + distance)
+                    availDrivers.push([distance.toFixed(2), record.cabid, record.latitude, record.longitude])
+                });
+                availDrivers = availDrivers.sort(_2dArraySort)
+                    // console.log(availDrivers)
+                let sortedDrivers = []
+                for (let i = 0; i < availDrivers.length; i++) {
+                    sortedDrivers.push({
+                        "cabid": availDrivers[i][1],
+                        "distance": availDrivers[i][0],
+                        "lat": availDrivers[i][2],
+                        "lng": availDrivers[i][3]
+                    })
+                }
+
+
+                // var sortedDriversJson = multiDimensionArray2JSON(availDrivers)
+                console.log(sortedDrivers)
+                res.render('nearestCabs.ejs', {
+                    drivers: sortedDrivers,
+                    rider: {
+                        "lat": userLat,
+                        "lng": userLng
+                    }
+                })
+
+            } catch (e) {
+                console.log(e)
+            }
 
 
 
-	}))
-    /*
-     *  Driver routes
-     */
+        }))
+        /*
+         *  Driver routes
+         */
 
 
-   
+
 
     app.get('/driver/:cabid?', (req, res) => {
         console.log("reached here" + req.params.cabid)
@@ -119,10 +132,6 @@ module.exports = (app) => {
             //flash error
         }
 
-        //check if the driver exists in db
-        let driver = await Driver.promise.findOne({
-            cabid: cab_id
-        })
 
         if (!driver) {
             let driver = new Driver()
@@ -133,12 +142,45 @@ module.exports = (app) => {
             res.redirect('/driver/' + cab_id)
             return
         } else {
-                driver.latitude = lat
-                driver.longitude = long
-                await driver.save()
-                res.redirect('/driver/' + cab_id)
+            driver.latitude = lat
+            driver.longitude = long
+            await driver.save()
+            res.redirect('/driver/' + cab_id)
         }
     }))
+
+    /*
+     * Rider routes
+     */
+
+    app.get('/rider', (req, res) => {
+        res.render("rider.ejs")
+    })
+
+    // app.post('/rider', function(req, res, next) {
+    //     console.log('Time:', Date.now())
+    //     next()
+    // },(req, res) => {
+    //     console.log("Reached post")
+    //     res.redirect('/')
+    // })
+
+
+
+      app.post('/rider', passport.authenticate('local-signup', {
+         successRedirect: '/findCabs',
+         failureRedirect: 'http://google.com',
+         failureFlash: true
+     }), function(err, req, res, next) {
+     	console.log("Error: " + err)
+     	res.redirect('http://google.com')
+    })
+
+    // app.post('/choseDriver/:driverId', isLoggedIn, (req, res) => {
+    // 	let driverId = req.params.driverId
+    // 	//server should call driver and pass the rider id
+    // })
+
 }
 
 
@@ -146,4 +188,3 @@ function parseAddressForGoogle(address) {
     return address.replace(/[\n\s]/g, "+");
 
 }
-
